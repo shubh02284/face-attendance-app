e0f4df0e449a031080646cb4b493232240439d00
shubham patidar <patidarshubham993@gmail.com>
2026-08-21 22:32:10 +0530

    Fix Python version for deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://face-attendance-app-eyro.onrender.com/api";
 
 const apiRequest = async (endpoint, options = {}) => {
   const token = localStorage.getItem("token");
