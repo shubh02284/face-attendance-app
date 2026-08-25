cfdf13ab069cae84584aa65cf16a0b9a962d0257
shubham patidar <patidarshubham993@gmail.com>
2026-08-26 02:29:54 +0530

    Fix production API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://face-attendance-app-eyro.onrender.com/api";
+const API_URL = "https://face-attendance-backend-f7e0.onrender.com";
 
 const apiRequest = async (endpoint, options = {}) => {
   const token = localStorage.getItem("token");
