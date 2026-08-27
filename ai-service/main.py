0e2f3ca86956a04f09152a08d77195eec345b725
shubham patidar <patidarshubham993@gmail.com>
2026-08-27 13:24:11 +0530

    Fix AI CORS

--- ai-service/main.py
+++ ai-service/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://face-attendance-swart.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
